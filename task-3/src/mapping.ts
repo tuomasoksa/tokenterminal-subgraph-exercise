3e1eb52f3c94082577c2480060e0ad1815a05c1a
tuomasoksa <tuomas.oksa@aalto.fi>
2021-12-15 19:04:35 +0200

    Add transferEvent/Task3 solution

--- task-3/src/mapping.ts
+++ task-3/src/mapping.ts
@@ -1,3 +1,4 @@
+import { Address } from '@graphprotocol/graph-ts'
 import { Transfer, ENSToken } from '../generated/ENSToken/ENSToken'
 import { TransferEvent } from '../generated/schema'
 
@@ -5,8 +6,25 @@ export function handleTransfer(event: Transfer): void {
   // Create a new TransferEvent entity based on the transaction hash of the event
   // let ...
 
+  // omatesti let transferEvent = Transfer.bind(event.address)
+  let id = event.transaction.hash.toHex()
+  let transferEvent = new TransferEvent(id)
+  let token = ENSToken.bind(event.address)
+
+
+
+
   // Populate transferEvent fields based on event metadata
   // transferEvent.timestamp = 
+  transferEvent.timestamp = event.block.timestamp.toI32()
+  transferEvent.id = event.transaction.hash.toHex()
+  transferEvent.block = event.block.number
+  transferEvent.fromAddress = event.params.from
+  transferEvent.toAddress = event.params.to
+  transferEvent.transferAmount = event.params.value
+
+  transferEvent.toBalance = token.balanceOf(event.params.to)
+  transferEvent.fromBalance = token.balanceOf(event.params.from)
 
 
   // Use the balanceOf public read-only function of the ENSToken 
@@ -16,8 +34,8 @@ export function handleTransfer(event: Transfer): void {
   // that emitted the event     
   // Check this section in The Graph docs:
   // https://thegraph.com/docs/developer/assemblyscript-api#access-to-smart-contract-state
-  
+
 
   // Save the entity to the store
-    
+  transferEvent.save()
 }
